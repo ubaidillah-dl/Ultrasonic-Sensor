418daeb1c53a8a4cb435f599a944c5a8d13c2779
Ubaidillah <107768826+ubaidillah-dl@users.noreply.github.com>
2022-06-29 20:56:18 +0700

    update

--- sensor_ultrasonic_client.ino
+++ sensor_ultrasonic_client.ino
@@ -1,54 +1,70 @@
 #include <ESP8266WiFi.h>
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
+#include <PubSubClient.h>
+#define MSG_BUFFER_SIZE  (50)
+#define trig 16                             
+#define echo 14                          
 
-/************************* Menentukan wifi *********************************/
-#define WLAN_SSID       "Warnet Putri"        // masukkan nama wifi
-#define WLAN_PASS       "poiuytrewq"          // masukkan password
+const char* ssid = "Warnet Putri";
+const char* password = "poiuytrewq";
+const char* mqtt_server = "192.168.100.117";
+int durasi,jarak;  
 
-/************************* Mengatur adafruit.io *********************************/
-#define MQTT_SERVER      "192.168.100.117"    // ip address raspberrypi
-#define MQTT_SERVERPORT  1883                 // port raspberrypi
-#define MQTT_USERNAME    ""                   // dikosongkan
-#define MQTT_KEY         ""                   // dikosongkan
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-/************************* Definisi pin sensor *********************************/
-#define trig 16                               // pin triger
-#define echo 14                               // pin echo
-short durasi;                                 // vaiabel
+char msg[MSG_BUFFER_SIZE];
 
-/************************* Deklarasi wifi server *********************************/
-WiFiClient client;
-//WiFiClientSecure client;                    // untuk koneksi lebih aman
-
-/************************* Mengatur mqtt client *********************************/
-Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_KEY);
-
-/************************* Publish data *********************************/
-Adafruit_MQTT_Publish ultra_stream = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME "sensor_ultrasonik");
-
-/*************************** Program ************************************/
-void setup() {
-  Serial.begin(115200);
+void setup_wifi() {
   delay(10);
-  
-  /*************************** Menentukan mode pin ************************************/
-  pinMode(trig,OUTPUT);
-  pinMode(echo,INPUT);
-  
-  /*************************** Menghubungkan ke wifi ************************************/
-  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
   }
-  Serial.println(" WiFi connected");
-  Serial.print("IP Address : "); 
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
+void callback(char* topic, byte* payload, unsigned int length){
+  for (int i = 0; i < length; i++) {
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    
+    if (client.connect(clientId.c_str())) {
+    } else {
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  setup_wifi();
+  Serial.begin(115200);
+  
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+ 
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
 void loop() {
-  MQTT_connect();
+  if (!client.connected()) {
+    reconnect();
+  }
+  
+  client.loop();
 
   digitalWrite(trig,LOW);
   delayMicroseconds(8);
@@ -56,36 +72,12 @@ void loop() {
   delayMicroseconds(8);
   digitalWrite(trig,LOW);
   delayMicroseconds(8);
-
+  
   durasi=pulseIn(echo,HIGH);
- 
-  Serial.print("sensor_ultrasonik ");
-  Serial.println(durasi);
-
-  /*************************** Mengirim data ke mqtt ************************************/
-  ultra_stream.publish(durasi);
+  jarak=(durasi/2)/28;
+  
+  snprintf (msg, MSG_BUFFER_SIZE, "Jarak Objek : %ld cm",jarak);
+  
+  client.publish("sensor_ult", msg);
   delay(1000);
-}
-
-void MQTT_connect(){
-  int8_t ret;
-
-  if (mqtt.connected()) {
-    return;
-  }
-
-  Serial.print("Connecting to MQTT... ");
-
-  uint8_t retries=3;
-  while ((ret=mqtt.connect())!=0){
-       Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 5 seconds...");
-       mqtt.disconnect();
-       delay(5000);
-       retries--;
-       if (retries==0) {
-         while (1);
-       }
-    }
-  Serial.println("MQTT Connected!");
 }
